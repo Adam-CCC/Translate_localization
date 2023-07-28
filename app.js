e6c0dbbc782d360890a93596b4b962ca7751f3cc
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-07-29 00:10:46 +0300

    app translated words is missingWords

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 const fs = require('fs');
 const readline = require('readline');
+const axios = require('axios');
+
+// Ваш IAM-токен от Yandex Cloud
+const IAM_TOKEN = 't1.9euelZqOl5SSk5jNmMqMz8uOl56Tju3rnpWaz5rJmZuPlYrHm5CMjYyKm4rl8_dVO3FZ-e9QRTBd_t3z9xVqbln571BFMF3-zef1656VmpyTmpDHx5aXzpPKz8vIy5bH7_zF656VmpyTmpDHx5aXzpPKz8vIy5bH.T5pT3O9CfJiUrAHqMvG1a2XfetxBd9dsk1ZY-FyusfDKC5z4x-tBgr1MoazxEK0m458fF-hthxcNo9bIzXhpBw';
+
+// Функция для отправки запроса к Yandex Translate API
+async function translateTexts(texts, targetLanguage, folderId) {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${IAM_TOKEN}`
+  };
+
+  const body = {
+    targetLanguageCode: targetLanguage,
+    texts,
+    folderId
+  };
+
+  try {
+    const response = await axios.post('https://translate.api.cloud.yandex.net/translate/v2/translate', body, { headers });
+    return response.data.translations.map(translation => translation.text);
+  } catch (error) {
+    console.error('Translation error:', error.message);
+    return [];
+  }
+}
+
+// Функция для чтения JSON-файла и перевода слов
+async function translateFromMissingWords(missingWords, targetLanguage, folderId) {
+  try {
+    const texts = Object.values(missingWords);
+
+    const translatedTexts = await translateTexts(texts, targetLanguage, folderId);
+
+    const translatedJson = {};
+    Object.keys(missingWords).forEach((key, index) => {
+      translatedJson[key] = translatedTexts[index];
+    });
+
+    console.log('Translated JSON:', translatedJson);
+  } catch (error) {
+    console.error('Error processing missing words:', error.message);
+  }
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -54,21 +98,16 @@ async function compareFiles() {
               }
             });
 
-            console.log('Кодовые слова, которых нет во втором файле:');
-            Object.entries(missingWords).forEach(([code, word]) => {
-              console.log(`${code}: ${word}`);
-            });
+            const targetLanguage = 'ru'; // Язык перевода, в данном случае, русский
+            const folderId = 'b1g2jfsjctrmjmpl626g'; // Замените на ваш реальный folder_id
 
-            const missingWordsFileName = 'missing_words.json'; // Имя файла для сохранения данных missingWords
-
-            const missingWordsData = JSON.stringify(missingWords, null, 2); // Преобразуем данные в формат JSON
-            await writeFile(missingWordsFileName, missingWordsData); // Записываем данные в файл
+            await translateFromMissingWords(missingWords, targetLanguage, folderId);
 
             // Запись измененных данных обратно во второй файл
             const updatedData2 = JSON.stringify(jsonData2, null, 2); // Преобразуем обновленные данные второго файла в формат JSON
             await writeFile(file2Path.trim(), updatedData2);
 
-            console.log(`Данные успешно сохранены в файле: ${missingWordsFileName}`);
+            console.log(`Данные успешно сохранены в файле: ${file2Path.trim()}`);
 
             rl.close();
           } catch (error) {
