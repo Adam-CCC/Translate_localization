b1d57dc837e1b7e1621f247cdce63a52b1479f34
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2023-07-31 14:30:40 +0300

    delete extra colors

--- app.js
+++ app.js
@@ -15,15 +15,9 @@ const rl = readline.createInterface({
 function printText(text, color) {
   const colors = {
     // Цвета текста
-    black: '\x1b[30m',
     red: '\x1b[31m',
     green: '\x1b[32m',
-    yellow: '\x1b[33m',
     blue: '\x1b[34m',
-    magenta: '\x1b[35m',
-    cyan: '\x1b[36m',
-    white: '\x1b[37m',
-
     // Сброс цвета
     reset: '\x1b[0m'
   };
